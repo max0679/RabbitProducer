5a5e48ee27c11143cd58f79dd9807b186f4bdd4e
max <maslenikov-maks1@mail.ru>
2025-05-12 13:21:55 +1000

    в качестве альтернативы добавили возможность выборки пользователя с БД H2, для этого был создан комонент MyUserDetailsManager

--- src/main/java/ru/maslenikov/manualtest1/config/SecurityFilterConfig.java
+++ src/main/java/ru/maslenikov/manualtest1/config/SecurityFilterConfig.java
@@ -4,6 +4,8 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
@@ -16,12 +18,31 @@ public class SecurityFilterConfig {
         return NoOpPasswordEncoder.getInstance();
     }
 
+    /***
+     *         2. sameOrigin:
+     • Этот параметр позволяет загружать фреймы только с того же источника (origin), что и основное приложение. "Происхождение" включает в себя протокол (HTTP/HTTPS), домен и порт.
+
+     • Это означает, что если ваше приложение работает на http://localhost:8080, то оно может загружать фреймы только с этого же адреса. Если вы попытаетесь загрузить фрейм с другого домена или порта, это будет заблокировано.
+
+     ▎Зачем это нужно в H2 Console?
+
+     H2 Console — это веб-интерфейс для работы с базой данных H2. Он часто загружается в виде фрейма. Если вы не разрешите загрузку фреймов из того же источника, браузер может заблокировать отображение H2 Console, и вы получите ошибку 403 (Forbidden).
+     ***/
+
     @Bean
     public SecurityFilterChain configure(HttpSecurity http)  throws Exception {
         http.httpBasic(Customizer.withDefaults());
         http.authorizeHttpRequests(
-                c -> c.anyRequest().authenticated()
+                c -> {
+                    c.anyRequest().authenticated();
+                }
+
         );
+        http
+            .csrf(AbstractHttpConfigurer::disable)
+            .headers((c -> {
+            c.frameOptions(HeadersConfigurer.FrameOptionsConfig::disable);
+        }));
         return http.build();
     }
 
