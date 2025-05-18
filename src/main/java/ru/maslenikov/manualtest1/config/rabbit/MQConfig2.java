ea07afe05d294bc653e3d7801bb1497c4312e506
max <maslenikov-maks1@mail.ru>
2025-05-18 15:21:41 +1000

    две очереди в одном приложении

--- src/main/java/ru/maslenikov/manualtest1/config/rabbit/MQConfig2.java
+++ src/main/java/ru/maslenikov/manualtest1/config/rabbit/MQConfig2.java
@@ -6,38 +6,29 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-public class MQConfig {
+public class MQConfig2 {
 
-    public static final String exchangeName = "testExchange1";
-    static final String queueName1 = "firstQueueEx1";
-    static final String queueName2 = "secondQueueEx1";
+    public static final String exchangeName = "testExchange2";
+    static final String queueName1 = "firstQueueEx2";
+    static final String queueName2 = "secondQueueEx2";
 
     // позволяет создать очередь если ее нет или подключиться к существующей
-    @Bean("first_queue")
+    @Bean("first_queue_ex2")
     public Queue myQueue1() {
         return new Queue(queueName1, true, false, false);
     }
 
-    @Bean("second_queue")
-    public Queue myQueue2() {
-        return new Queue(queueName2, true, false, false);
-    }
-
     // точкой обмена, распределяет сообщения между очередями на основе связей (bindings)
-    @Bean("ex1")
+    @Bean("ex2")
     public Exchange myExchange() {
         return new DirectExchange(exchangeName, true, false);
     }
 
     // создает связь между Exchange и Queue
-    @Bean("binding_first_queue")
-    public Binding myBinding1(@Qualifier("first_queue") Queue myQueue, @Qualifier("ex1") Exchange myExchange) {
+    @Bean("binding_first_queue_ex2")
+    public Binding myBinding1(@Qualifier("first_queue_ex2") Queue myQueue, @Qualifier("ex2") Exchange myExchange) {
         return BindingBuilder.bind(myQueue).to(myExchange).with("first_key").noargs();
     }
 
-    @Bean("binding_second_queue")
-    public Binding myBinding2(@Qualifier("second_queue") Queue myQueue, @Qualifier("ex1") Exchange myExchange) {
-        return BindingBuilder.bind(myQueue).to(myExchange).with("second_key").noargs();
-    }
 
 }
